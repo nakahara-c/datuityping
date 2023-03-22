ca7f39efff10f1414186ea62d02a188f3845879a
nakahara-c <ttcocoan@gmail.com>
2023-03-22 21:37:38 +0900

    ツイートボタン追加 タグなし

--- main.js
+++ main.js
@@ -363,6 +363,23 @@ function typeFinish(isCompleted) {
 
     }
 
-    //makeTweet();
+    makeTweet();
 
+}
+
+function makeTweet (level) {
+    //const playingLevel = level;
+    const playingLevel = 'LEVEL 1';
+    const tweetButton = document.getElementById('tweet');
+    
+    const t = String((30 - parseFloat(timer.textContent)).toFixed(1));
+
+    const c = count.textContent;
+    const k = kpm.textContent;
+    //const hashTags = "脱衣タイピング"
+    const tweet = `${playingLevel} cleared! ${c}keys in ${t} sec (${k}KPM) ＠脱衣タイピング（α）`;
+    const url = 'https://datuityping.x.fc2.com/';
+
+    const tweetText = `https://twitter.com/intent/tweet?ref_src=twsrc&text=${tweet}&url=${url}`;
+    tweetButton.href = tweetText;
 }
