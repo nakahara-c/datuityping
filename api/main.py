238d3e0ca1c2aa389e839040940e7374c7e30b72
nakahara-c <ttcocoan@gmail.com>
2023-05-02 02:50:38 +0900

    画像ID返すAPI

---
+++ api/main.py
@@ -0,0 +1,22 @@
+import os
+import random
+from flask import jsonify, request
+
+values_set = {
+    1: [10, 20, 30],
+    2: [40, 50, 60],
+    3: [70, 80, 90],
+    4: [100, 110, 120],
+    5: [130, 140, 150]
+}
+
+def datui_randomImgID(request):
+    input_value = int(request.args.get('input', 0))
+
+    if input_value < 1 or input_value > 5:
+        return "Invalid input. Must be between 1 and 5.", 400
+
+    values = values_set[input_value]
+    random_value = random.choice(values)
+
+    return jsonify({"randomValue": random_value}), 200
