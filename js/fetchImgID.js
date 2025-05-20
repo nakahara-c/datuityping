9f7bab110154f0b9de40c4671bf614585ce91fe6
nakahara-c <ttcocoan@gmail.com>
2025-05-20 22:12:02 +0900

    default cnt

--- js/fetchImgID.js
+++ js/fetchImgID.js
@@ -1,6 +1,6 @@
 const API_ENDPOINT = 'https://us-west1-nkhr-c.cloudfunctions.net/dr';
 
-export async function fetchImgID(level, cnt) {
+export async function fetchImgID(level, cnt = 0) {
     const response = await fetch(`${API_ENDPOINT}?input=${level}&cnt=${cnt}`);
     const data = await response.json();
     return data;
